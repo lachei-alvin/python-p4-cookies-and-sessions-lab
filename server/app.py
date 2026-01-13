8086ce35bdea5672cf4c29b3a125a94efb53f1b8
alvin koech <alvinkoech2020@gmail.com>
2026-01-13 15:57:47 +0300

    added files

--- server/app.py
+++ server/app.py
@@ -6,29 +6,50 @@ from flask_migrate import Migrate
 from models import db, Article, User
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/clear')
+
+@app.route("/clear")
 def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    session["page_views"] = 0
+    return make_response(
+        jsonify({"message": "200: Successfully cleared session data."}), 200
+    )
 
-@app.route('/articles')
+
+@app.route("/articles")
 def index_articles():
+    articles = Article.query.all()
+    return make_response(jsonify([article.to_dict() for article in articles]), 200)
 
-    pass
 
-@app.route('/articles/<int:id>')
+@app.route("/articles/<int:id>")
 def show_article(id):
+    # Initialize page_views if not already set
+    session["page_views"] = session["page_views"] if "page_views" in session else 0
 
-    pass
+    # Increment page views
+    session["page_views"] += 1
 
-if __name__ == '__main__':
+    # Enforce page view limit
+    if session["page_views"] > 3:
+        return make_response(
+            jsonify({"message": "Maximum pageview limit reached"}), 401
+        )
+
+    article = Article.query.get(id)
+
+    if not article:
+        return make_response(jsonify({"error": "Article not found"}), 404)
+
+    return make_response(jsonify(article.to_dict()), 200)
+
+
+if __name__ == "__main__":
     app.run(port=5555)
